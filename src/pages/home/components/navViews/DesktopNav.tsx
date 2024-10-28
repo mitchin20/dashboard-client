682dbfc2f61ae07108151444a985169cf369002f
Mit Chin <mitchin10@gmail.com>
2024-10-28 14:18:50 -0400

    Update desktop nav style

--- src/pages/home/components/navViews/DesktopNav.tsx
+++ src/pages/home/components/navViews/DesktopNav.tsx
@@ -4,13 +4,13 @@ import { DesktopNavProps } from "../../../../types";
 const DesktopNav: React.FC<DesktopNavProps> = ({ isView, navItems }) => {
     return (
         <div
-            className={`hidden w-full md:flex md:items-center md:space-x-6 space-y-4 md:space-y-0 md:w-auto mt-4 md:mt-0`}
+            className={`hidden w-full md:flex md:items-center md:space-x-12 space-y-4 md:space-y-0 md:w-auto mt-4 md:mt-0`}
         >
             {navItems.map((item, index) => (
                 <a
                     key={index}
                     href={item.href}
-                    className="text-gray-700 font-medium hover:text-[#082f49] hover:scale-150 px-4 py-4 rounded-xl transition-transform duration-200 ease-in-out"
+                    className="text-gray-700 font-medium hover:text-[#082f49] hover:font-extrabold hover:scale-150 px-4 py-4 transition-transform duration-500 ease-in-out"
                 >
                     {item.label}
                 </a>
