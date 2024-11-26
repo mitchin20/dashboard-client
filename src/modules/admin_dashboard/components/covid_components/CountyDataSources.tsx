5b8902ea4f6b512310931e704fabf86eccce9b44
Mit Chin <mitchin10@gmail.com>
2024-11-26 00:44:53 -0500

    Update CountyDataSource. Conditional rendering if data available

--- src/modules/admin_dashboard/components/covid_components/CountyDataSources.tsx
+++ src/modules/admin_dashboard/components/covid_components/CountyDataSources.tsx
@@ -14,29 +14,33 @@ const CountyDataSources: React.FC<CountyMetricsProps> = ({ data, loading }) => {
         data?.annotations?.vaccinationsAdditionalDose?.sources[0];
 
     return (
-        <div className="mt-10">
-            <div className="flex font-bold mb-3">
-                <ApacheIcon className="w-6 h-6 mr-3" />
-                <h1>Data Sources</h1>
+        casesAndDeathsSource &&
+        hospitalizationsSource &&
+        vaccinationSource && (
+            <div className="mt-10">
+                <div className="flex font-bold mb-3">
+                    <ApacheIcon className="w-6 h-6 mr-3" />
+                    <h1>Data Sources</h1>
+                </div>
+
+                <div className="flex flex-col ml-3 italic mb-10 text-xs gap-y-2">
+                    <h4>Cases and Deaths</h4>
+                    <Link href={casesAndDeathsSource?.url[1]} target="_blank">
+                        {casesAndDeathsSource?.name}
+                    </Link>
+
+                    <h4>Hospitalizations</h4>
+                    <Link href={hospitalizationsSource?.url} target="_blank">
+                        {hospitalizationsSource?.name}
+                    </Link>
+
+                    <h4>Vaccination Data</h4>
+                    <Link href={vaccinationSource?.url} target="_blank">
+                        {vaccinationSource?.name}
+                    </Link>
+                </div>
             </div>
-
-            <div className="flex flex-col ml-3 italic mb-10 text-xs gap-y-2">
-                <h4>Cases and Deaths</h4>
-                <Link href={casesAndDeathsSource.url[1]} target="_blank">
-                    {casesAndDeathsSource.name}
-                </Link>
-
-                <h4>Hospitalizations</h4>
-                <Link href={hospitalizationsSource.url} target="_blank">
-                    {hospitalizationsSource.name}
-                </Link>
-
-                <h4>Vaccination Data</h4>
-                <Link href={vaccinationSource.url} target="_blank">
-                    {vaccinationSource.name}
-                </Link>
-            </div>
-        </div>
+        )
     );
 };
 
