b51681b8a715cf52ff79cb112b857ab1c62e63ff
Mit Chin <mitchin10@gmail.com>
2024-12-02 20:25:30 -0500

    Update styles

--- src/modules/admin_dashboard/components/bingo_game_components/GameBoard.tsx
+++ src/modules/admin_dashboard/components/bingo_game_components/GameBoard.tsx
@@ -19,13 +19,13 @@ const GameBoard: React.FC<GameBoardProps> = ({
                     <h4 className="text-xl text-blue-700 font-bold mb-2">
                         {letter}
                     </h4>
-                    <div className={`grid grid-cols-3 md:grid-cols-2 gap-2`}>
+                    <div className={`grid grid-cols-2 md:grid-cols-3 gap-2`}>
                         {gameNumbers
                             .slice(index * 15, (index + 1) * 15)
                             .map((number) => (
                                 <div
                                     key={number}
-                                    className={`w-10 h-10 flex items-center justify-center text-sm border border-blue-200 rounded-full ${rolledNumbers.includes(number) ? "bg-red-300 text-white" : "bg-white text-gray-400"} xl:w-14 xl:h-14 xl:text-lg`}
+                                    className={`w-8 h-8 flex items-center justify-center text-sm border border-blue-200 rounded-full ${rolledNumbers.includes(number) ? "bg-red-300 text-white" : "bg-white text-gray-400"} xl:w-14 xl:h-14 xl:text-lg`}
                                 >
                                     {number}
                                 </div>
