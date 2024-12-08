dfa09b1e743eaa9b9ccfba426eab190724096058
Mit Chin <mitchin10@gmail.com>
2024-12-08 11:27:24 -0500

    Remove testing code

--- src/modules/admin_dashboard/components/DashboardSummary.tsx
+++ src/modules/admin_dashboard/components/DashboardSummary.tsx
@@ -2,7 +2,6 @@ import React, { lazy, useContext } from "react";
 import { DashboardSummaryProps } from "../../../types";
 import RecentVisited from "./dashboard_components/RecentVisited";
 import { ThemeContext } from "../../../context/ThemeContext";
-import LoadingIcon2 from "../../../svgIcons/LoadingIcon2";
 
 const Section1 = lazy(() => import("./dashboard_components/Section1"));
 const Section2 = lazy(() => import("./dashboard_components/Section2"));
@@ -13,9 +12,6 @@ const DashboardSummary: React.FC<DashboardSummaryProps> = () => {
     return (
         <div className={`mt-5 ${textColor}`}>
             <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-5">
-                <div className="p-4 rounded-lg bg-gray-400 flex items-center justify-center">
-                    <LoadingIcon2 className="w-20 h-20 animate-spin" />
-                </div>
                 <Section1 />
                 <Section2 />
             </div>
